b76ec28bf11485e357ab594749aea98137d40568
yassine <med.yassine.kharrat@gmail.com>
2024-07-21 19:29:37 +0100

    nothing changed here

--- SoretrakFront/src/features/plannification/store/trips.module.js
+++ SoretrakFront/src/features/plannification/store/trips.module.js
@@ -1,9 +1,13 @@
 import myApi from "@/services/myApi.service.js"
+import { ResourceArray } from "../utils/Resource";
 
 export const tripsModule={
     namespaced: true,
     state: {
       trips:[],
+      ligneFromTrips: new ResourceArray(),
+      agentFromTrips: new ResourceArray(),
+      vehiculeFromTrips: new ResourceArray(),
       loading:true,
       error:null
     },
@@ -16,7 +20,16 @@ export const tripsModule={
       },
       setError(state,err){
         state.error =err;
-      }
+      },
+      setLignes(state,lignes){
+        state.ligneFromTrips=lignes;
+      },
+      setAgents(state,agents){
+        state.agentFromTrips=agents;
+      },
+      setVehicules(state,vehicules){
+        state.vehiculeFromTrips=vehicules;
+      },
     },
     actions: {
       async getTrips({ commit }, date) {
@@ -31,5 +44,18 @@ export const tripsModule={
         }
       }
     },
-    getters: {},
+    getters: {
+      getLigneById:(state)=>(id)=>{
+        return state.ligneFromTrips.getResource(id);
+      },
+
+      getAgentById:(state)=>(id)=>{
+        return state.agentFromTrips.getResource(id);
+      },
+
+      getVehiculeById:(state)=>(id)=>{
+        return state.vehiculeFromTrips.getResource(id);
+      },
+
+    },
   }
